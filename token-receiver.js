021d820a2db279939e4e29ac979ca42d5c2dbb22
Viral Bhadeshiya <viralrbhadeshiya@gmail.com>
2022-10-03 17:45:39 +0530

    changes realted to new redis emitter code

--- token-receiver.js
+++ token-receiver.js
@@ -1,30 +1,34 @@
 const axios = require('axios').default;
-const WebSocketClient = require('websocket').client;
+const { io } = require('socket.io-client')
 
 const relysiaEndpoint = 'api.relysia.com';
 
 (async () => {
     // Login into account
-    const loginObject = await axios.post(`https://${relysiaEndpoint}/v1/auth`, {email: "test14@vaionex.com", password: "123456"});
+    const loginObject = await axios.post(`http://${relysiaEndpoint}/v1/auth`, {email: "viral@vaionex.com", password: "4m4z1ngT3ch"});
     console.log('Login completed', loginObject.data);
     // Connect websocket for this account
-    const client = new WebSocketClient();
-    client.connect(`wss://${relysiaEndpoint}/websocket-connection`, null, null, {authToken: loginObject.data.data.token});
+    const socket = io(`ws://${relysiaEndpoint}`, {
+        extraHeaders: {
+            authToken: loginObject.data.data.token,
+        },
+    });
 
-    client.on('connect', function(connection) {
+    socket.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+
+    // Listen for messages and log them as notification arrive
+    socket.on('notification', function (message) {
+        console.log('event received', message);
+    })
+
+    socket.on('connect', function(connection) {
         console.log('WebSocket Client Connected');
 
-        connection.on('error', function(error) {
-            console.log("Connection Error: " + error.toString());
-        });
-        connection.on('close', function() {
+        
+        socket.on('close', function() {
             console.log('echo-protocol Connection Closed');
-        });
-
-        // Listen for messages and log them as notification arrive
-        connection.on('message', function(message) {
-            const data = JSON.parse(message.utf8Data);
-            console.log('Socket event received: ', data);
         });
     });
 })()
